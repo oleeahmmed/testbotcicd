85c080eee22a91b2a6226f4dd9ec2643f38e3eab
oleeahmmed <ashik.mou.907@gmail.com>
2025-07-11 22:21:04 +0600

    add

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def run():
     try:
         with sync_playwright() as p:
             # Launch browser with user-agent to mimic a real browser
-            browser = p.chromium.launch(headless=False)  # Set to False for debugging
+            browser = p.chromium.launch(headless=True)  # Set to False for debugging
             context = browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             )
